a31276dacdcc891de7589b8c887afd2b4c6da5f3
Jepjep18 <jeparnado@gmail.com>
2024-04-23 09:09:00 +0800

    hehe

--- resources/views/doctrack/index.blade.php
+++ resources/views/doctrack/index.blade.php
@@ -26,38 +26,42 @@
                                 </tr>
                             </thead>
                             <tbody>
-                                @foreach ($documents as $document)
-                                    <tr>
-                                        <td class="py-2">{{ $document->department }}</td>
-                                        <td class="py-2">{{ $document->personnel }}</td>
-                                        <td class="py-2">
-                                            <a href="{{ asset('upload/' . $document->file_name) }}" download class="text-blue-600 hover:text-blue-800 font-semibold">{{ $document->file_name }}</a>
-                                        </td>
-                                        <td class="py-2">{{ $document->created_at }}</td>
-                                        <td class="py-2">{{ $document->acceptance ? $document->acceptance->accepted_at : 'Not Accepted Yet' }}</td>
-                                        <td class="py-2">
-                                            @if ($document->acceptance && $document->acceptance->reuploaded_file_name)
-                                                <a href="{{ route('download.reupload', ['file' => $document->acceptance->reuploaded_file_name]) }}" class="text-blue-600 hover:text-blue-800 font-semibold download-link" download>{{ $document->acceptance->reuploaded_file_name }}</a>
-                                            @else
-                                                No Reuploaded File
-                                            @endif
-                                        </td>
-                                        
-                                        <td class="py-2">{{ $document->acceptance ? $document->acceptance->updated_at : 'Not Released Yet' }}</td>
-                                        <td class="py-2">
-                                            @if ($document->department && $document->personnel && $document->file_name && $document->created_at && $document->acceptance && $document->acceptance->accepted_at && $document->acceptance->reuploaded_file_name && $document->acceptance->updated_at)
-                                                <button class="bg-green-500 text-white font-semibold px-4 py-2 rounded">Done</button>
-                                            @else
-                                                <button class="bg-red-500 text-white font-semibold px-4 py-2 rounded">Pending</button>
-                                            @endif
-                                        </td>
-                                        <td class="py-2 text-left">
-                                            <div style="display: flex; justify-content: center;">
-                                                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded edit-button" id="editButton{{$document->id}}" onclick="location.href='{{ route('doctrack.edit', $document->id) }}'">Edit</button>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                @endforeach
+                            @foreach ($documents as $document)
+                                <tr>
+                                    <td class="py-2">{{ $document->department }}</td>
+                                    <td class="py-2">{{ $document->personnel }}</td>
+                                    <td class="py-2">
+                                        <a href="{{ asset('upload/' . $document->file_name) }}" download class="text-blue-600 hover:text-blue-800 font-semibold">{{ $document->file_name }}</a>
+                                    </td>
+                                    <td class="py-2">{{ $document->created_at }}</td>
+                                    <td class="py-2 @if (!$document->acceptance || !$document->acceptance->accepted_at) text-red-500 @endif">
+                                        {{ $document->acceptance ? $document->acceptance->accepted_at : 'Not Accepted Yet' }}
+                                    </td>
+                                    <td class="py-2">
+                                        @if ($document->acceptance && $document->acceptance->reuploaded_file_name)
+                                            <a href="{{ route('download.reupload', ['file' => $document->acceptance->reuploaded_file_name]) }}" class="text-blue-600 hover:text-blue-800 font-semibold download-link" download>{{ $document->acceptance->reuploaded_file_name }}</a>
+                                        @else
+                                            <span class="@if (!$document->acceptance || !$document->acceptance->reuploaded_file_name) text-red-500 @endif">No Reuploaded File</span>
+                                        @endif
+                                    </td>
+                                    <td class="py-2 @if (!$document->acceptance || !$document->acceptance->updated_at) text-red-500 @endif">
+                                        {{ $document->acceptance ? $document->acceptance->updated_at : 'Not Released Yet' }}
+                                    </td>
+                                    <td class="py-2">
+                                        @if ($document->department && $document->personnel && $document->file_name && $document->created_at && $document->acceptance && $document->acceptance->accepted_at && $document->acceptance->reuploaded_file_name && $document->acceptance->updated_at)
+                                            <button class="bg-green-500 text-white font-semibold px-4 py-2 rounded">Done</button>
+                                        @else
+                                            <button class="bg-red-500 text-white font-semibold px-4 py-2 rounded">Pending</button>
+                                        @endif
+                                    </td>
+                                    <td class="py-2 text-left">
+                                        <div style="display: flex; justify-content: center;">
+                                            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded edit-button" id="editButton{{$document->id}}" onclick="location.href='{{ route('doctrack.edit', $document->id) }}'">Edit</button>
+                                        </div>
+                                    </td>
+                                </tr>
+                            @endforeach
+
                             </tbody>
                         </table>
                     </div>
